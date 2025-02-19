471fc813f69f2734d2ca7945b826603161cc3128
UlfatJarin <richiulfat@gmail.com>
2025-02-20 01:38:59 +0600

    9th

--- src/components/Banner2.jsx
+++ src/components/Banner2.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import Button from './layers/Button'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Banner2 = () => {
+
+    const navigate =useNavigate()
     return (
-        <div className='flex justify-between bg-primary rounded-xl relative'>
-            <div className='py-24 px-28'>
-                <p className='max-w-[640px] text-whitec text-[52px] font-Outfit font-bold leading-[90px]'>Book Appointment<br/> With 100+ Trusted Doctors</p>
-                <Button classname='!bg-whitec !text-text1' btntext='Create account' />
+        <div className='pt-12 md:pt-16 lg:py-24 px-10 md:px-16 lg:px-28 flex flex-col lg:flex-row justify-between bg-primary rounded-xl relative mt-40 mb-20'>
+            <div className='w-full lg:w-1/2 '>
+                <h2 className='max-w-[640px] text-whitec text-[52px] font-Outfit font-bold leading-[90px] mb-9'>Book Appointment<br/> With 100+ Trusted Doctors</h2>
+                <Button onClick={()=>{navigate('/login');scrollTo(0,0)} }classname='!bg-whitec !text-text1' btntext='Create account' />
             </div>
-            <div className='absolute right-0 bottom-0 '>
-                <img className='w-[437px]  ' src={assets.appointment_img} alt="" />
+            <div className=' lg:absolute right-28 bottom-0 '>
+                <img className='max-w-[460px]  ' src={assets.appointment_img} alt="" />
             </div>
         </div>
     )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+    return (
+        <div className='mt-16'>
+            <div className='grid grid-cols-4 py-16'>
+                <div className='col-span-2'>
+                    <img src={assets.logo} alt="" />
+                    <p className='max-w-[646px] font-Outfit text-lg leading-7 text-text2 mt-10'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+                </div>
+                <div>
+                    <h5 className='font-Outfit font-semibold text-[22px] leading-7 text-text2 mb-10' >COMPANY</h5>
+                    <ul className='font-Outfit text-lg leading-7 text-text2 flex flex-col gap-2'>
+                        <li>Home </li>
+                        <li>About us </li>
+                        <li>Contact us </li>
+                        <li>Privacy policy </li>
+                    </ul>
+                </div>
+                <div>
+                    <h5 className='font-Outfit font-semibold text-[22px] leading-7 text-text2 mb-10'>GET IN TOUCH</h5>
+                    <ul className='font-Outfit text-lg leading-7 text-text2 flex flex-col gap-2'>
+                        <li>+1-212-456-7890</li>
+                        <li>greatstackdev@gmail.com</li>
+                    </ul>
+
+                </div>
+
+            </div>
+            <div className='py-7 border-t border-[#BDBDBD]'>
+                <p className='font-Outfit text-lg leading-7 text-text2 text-center '>Copyright © 2024 GreatStack - All Right Reserved.</p>
+            </div>
+        </div>
+
+    )
+}
+
+export default Footer
